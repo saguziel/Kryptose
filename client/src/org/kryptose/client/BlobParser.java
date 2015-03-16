82863ce64a4c3a0ccceee646f08a111a0e60a2f1
Jeff Tian <yt336@cornell.edu>
2015-03-15 22:42:22 -0400

    starting blob parsing interface

--- client/src/org/kryptose/client/BlobParser.java
+++ client/src/org/kryptose/client/BlobParser.java
@@ -6,7 +6,12 @@ import java.util.ArrayList;
 /**
  * Created by jeff on 3/15/15.
  */
-public interface BlobParser {
+public class PasswordFile {
+
+    
+    public PasswordFile(Blob b) {
+
+    }
 
     public ArrayList<Credential> decryptBlob(String key, Blob b);
     public Blob encryptBlob(String key, ArrayList<Credential> l);
