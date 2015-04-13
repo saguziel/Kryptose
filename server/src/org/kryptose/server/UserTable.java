1ab55f20721aa17e6be272aea8f3b7a5b4822ddf
AMarcedone <AMarcedone@users.noreply.github.com>
2015-04-13 03:01:40 -0400

    Finished UserTable class for authentication (calls need to be added in the rest of the code. Look at the main of the class for usage examples

--- server/src/org/kryptose/server/UserTable.java
+++ server/src/org/kryptose/server/UserTable.java
@@ -6,6 +6,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -17,8 +18,8 @@ public class UserTable {
 	
 	private class UserRecord{
 		final String username;
-		String salt;
-		String auth_key_hash;
+		byte[] salt;
+		byte[] auth_key_hash;
 		
 		//TODO: for final
 		int login_attempts;
@@ -28,7 +29,7 @@ public class UserTable {
 			
 			try {
 				
-				final byte[] salt = new byte[salt_size];
+				salt = new byte[salt_size];
 		    	SecureRandom rnd;
 
 	    		rnd = SecureRandom.getInstance("SHA1PRNG");
@@ -40,39 +41,110 @@ public class UserTable {
 				
 				//TODO handle auth_key... perhaps zero it...
 				
-				byte[] auth_key_hash = factory.generateSecret(spec).getEncoded();
+				auth_key_hash = factory.generateSecret(spec).getEncoded();
 
 		    	
 
-			} catch (NoSuchAlgorithmException e) {
+			} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 				throw new CryptoPrimitiveNotSupportedException(e);
 			}
+		}
+		
+		public boolean authenticate(byte[] tentative_key){
+			try{
+				SecretKeyFactory factory;
+				factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+				KeySpec spec = new PBEKeySpec(DatatypeConverter.printHexBinary(tentative_key).toCharArray(), salt, 65536, 256);
+			
+				//TODO: secure erease.
+				DatatypeConverter.printHexBinary(auth_key_hash);
+				DatatypeConverter.printHexBinary(factory.generateSecret(spec).getEncoded());
+				
+				return Arrays.equals(auth_key_hash, factory.generateSecret(spec).getEncoded());
+			} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new CryptoPrimitiveNotSupportedException(e);
+			}
+
+		}
+
+		public boolean changeUserAuthKey(byte[] old_key, byte[] new_key) {
+			if(!authenticate(old_key))
+				return false;
+
+			try {
+				
+				salt = new byte[salt_size];
+		    	SecureRandom rnd;
+
+	    		rnd = SecureRandom.getInstance("SHA1PRNG");
+		    	rnd.nextBytes(salt);
+
+		    	SecretKeyFactory factory;
+				factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+				KeySpec spec = new PBEKeySpec(DatatypeConverter.printHexBinary(new_key).toCharArray(), salt, 65536, 256);
+				
+				//TODO handle auth_key... perhaps zero it...
+				
+				auth_key_hash = factory.generateSecret(spec).getEncoded();
+
+		    	return true;
+
+			} catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				throw new CryptoPrimitiveNotSupportedException(e);
+			}
+
+			
 		}
 
 				
 	}
 	
 	int salt_size;
-	private Hashtable<String,UserRecord> Users;
+	private ConcurrentHashMap<String,UserRecord> Users;
 	
-	public enum result{USER_NOT_FOUND, USER_ALREADY_EXISTS, USER_ADDED, WRONG_CREDENTIALS, AUTHENTICATION_SUCCESS};
+	public enum result{USER_NOT_FOUND, USER_ALREADY_EXISTS, USER_ADDED, WRONG_CREDENTIALS, AUTHENTICATION_SUCCESS, AUTH_KEY_CHANGED};
 
 	public boolean contains(String username){
 		return Users.containsKey(username);
 	}
 	
 	public result addUser(String username, byte[] auth_key){
-		if this.contains(username) return USER_ALREADY_EXISTS;
+		if (this.contains(username)) 
+			return result.USER_ALREADY_EXISTS;
 		
-		
+		Users.put(username, new UserRecord(username, auth_key));
+		return result.USER_ADDED;
+	}
+	
+	public result auth(String username, byte[] auth_key){
+		if (!this.contains(username)) 
+			return result.USER_NOT_FOUND;
+		else if(Users.get(username).authenticate(auth_key))
+			return result.AUTHENTICATION_SUCCESS;
+		else 
+			return result.WRONG_CREDENTIALS;
+	}
+	
+	public result changeAuthKey(String username, byte[] old_key, byte[] new_key){
+		if (!this.contains(username)) 
+			return result.USER_NOT_FOUND;
+		else if(Users.get(username).changeUserAuthKey(old_key,new_key))	
+			return result.AUTH_KEY_CHANGED;
+		else 
+			return result.WRONG_CREDENTIALS;
+
 	}
 	
 	
 	public UserTable(int salt_size) {
 		this.salt_size = salt_size;
-		Users = new Hashtable<String,UserRecord>();
+		Users = new ConcurrentHashMap<String,UserRecord>();
 //		Users.put("me", new UserRecord("me", "AAAAAAAAAAAAAAAA", "A"));
 	}
 	
@@ -80,7 +152,26 @@ public class UserTable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		UserTable u = new UserTable();
+		UserTable u = new UserTable(50);
+		
+		byte[] good_pwd = "good".getBytes();
+		byte[] bad_pwd = "bad".getBytes();
+		
+		System.out.println(u.contains("Antonio"));
+		u.addUser("Antonio", good_pwd);
+		System.out.println(u.contains("Antonio"));
+		System.out.println(u.contains("AntonioAAAA"));
+		
+		System.out.println(u.auth("Mario", good_pwd));		
+		System.out.println(u.auth("Antonio", good_pwd));		
+		System.out.println(u.auth("Antonio", bad_pwd));		
+		
+		System.out.println(u.changeAuthKey("Mario", good_pwd, good_pwd));		
+		System.out.println(u.changeAuthKey("Antonio", good_pwd,bad_pwd));		
+		System.out.println(u.auth("Antonio", good_pwd));		
+		System.out.println(u.auth("Antonio", bad_pwd));		
+		
+		
 	}
 
 }
