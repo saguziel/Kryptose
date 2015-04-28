644d491a70f57b5e54f9ffcb4c0ca51b4314e931
Jeff Tian <yt336@cornell.edu>
2015-04-27 21:14:38 -0400

    new view

---
+++ client/src/org/kryptose/client/OldView.java
@@ -0,0 +1,29 @@
+
+package org.kryptose.client;
+
+
+
+
+public abstract class OldView {
+
+    abstract void promptUserName();
+
+	abstract void promptPassword();
+
+    abstract void promptCmd();
+
+	abstract void promptCmd(String s);
+
+	abstract void logout();
+
+    abstract void promptStart();
+
+    abstract void displayMessage(String s);
+
+    abstract void createUsername();
+
+    abstract void createPass();
+
+    abstract void set();
+
+}
